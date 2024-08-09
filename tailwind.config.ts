b74ca9740f1004e4b42804953aab52f74682c676
selly361 <seikou615@gmail.com>
2024-08-09 23:10:41 +0100

    Update theme with `colors`, `fontSize` and `backgroundImage`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,38 @@ const config: Config = {
 	],
 	theme: {
 		extend: {
+			colors: {
+				grey: {
+					500: '#86A2A5',
+					900: '#2A4144'
+				},
+				green: {
+					200: '#E0F1E8',
+					600: '#0C7D69'
+				},
+				red: '#D73C3C'
+			},
+			fontSize: {
+				'heading': [
+					'32px',
+					{
+						lineHeight: '100%',
+						letterSpacing: '-1px',
+						fontWeight: '700'
+					}
+				],
+				'body-m': [
+					'18px',
+					{
+						lineHeight: '150%',
+						letterSpacing: '0px',
+						fontWeight: '400'
+					}
+				]
+			},
+			backgroundImage: {
+				'checkbox': "url('/icon-checkbox-check.svg')"
+			}
 		}
 	},
 	plugins: []
