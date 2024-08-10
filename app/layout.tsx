9928f7dd0658114f57413391434b2028265e59f2
selly361 <seikou615@gmail.com>
2024-08-10 21:45:03 +0100

    Wrap children with `FormProvider`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import './globals.css'
 
+import { FormProvider } from '@/context'
 import { Karla } from 'next/font/google'
 import { Metadata } from 'next'
 import { PropsWithChildren } from 'react'
+import clsx from 'clsx'
 
 const karla = Karla({ subsets: ['latin'] })
 
@@ -15,7 +17,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
 	return (
 		<html lang='en'>
-			<body className={karla.className}>{children}</body>
+			<body className={clsx(karla.className, 'bg-green-200 flex items-center justify-center min-h-screen py-8')}>
+				<FormProvider>{children}</FormProvider>
+			</body>
 		</html>
 	)
 }
